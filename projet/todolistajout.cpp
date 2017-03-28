1b02f5c755d24ccb6876bc9fec2ce97f0c1f4051
Sallio <romane.sallio@gmail.com>
2017-03-28 15:14:36 +0200

    poo 28/03 15h14

--- projet/todolistajout.cpp
+++ projet/todolistajout.cpp
@@ -2,15 +2,38 @@
 
 ToDoListAjout::ToDoListAjout(QWidget *parent) : QWidget(parent)
 {
-    titre = new QLineEdit;
-    objectif = new QLineEdit;
-    date = new QLineEdit;
+    initLine();
+    initButton();
+    initLayout();
+}
 
-    layout = new QFormLayout;
+void ToDoListAjout::initLine(){
+    _titre = new QLineEdit;
+    _objectif = new QLineEdit;
+    _date = new QDateEdit;
+}
 
-    layout->addRow("Titre : ", titre);
-    layout->addRow("Votre objectif : ", objectif);
-    layout->addRow("Date : ", date);
+void ToDoListAjout::initButton(){
+    _quitBtn = new QPushButton("Quitter",this);
+    _validerBtn = new QPushButton("Valider",this);
 
-    setLayout(layout);
+    QObject::connect(_quitBtn, SIGNAL(clicked(bool)),this, SLOT(hide()));
+}
+
+void ToDoListAjout::initLayout(){
+    _layoutBtn = new QHBoxLayout;
+    _layoutLine = new QFormLayout;
+    _layoutPrincipal = new QVBoxLayout;
+
+    _layoutBtn->addWidget(_validerBtn);
+    _layoutBtn->addWidget(_quitBtn);
+
+    _layoutLine->addRow("Titre : ", _titre);
+    _layoutLine->addRow("Votre objectif : ", _objectif);
+    _layoutLine->addRow("Date : ", _date);
+
+    _layoutPrincipal->addLayout(_layoutLine);
+    _layoutPrincipal->addLayout(_layoutBtn);
+
+    setLayout(_layoutPrincipal);
 }
--- projet/todolistajout.h
+++ projet/todolistajout.h
@@ -3,7 +3,11 @@
 
 #include <QFormLayout>
 #include <QLineEdit>
+#include <QDateEdit>
+#include <QPushButton>
 #include <QWidget>
+#include <QObject>
+#include <QApplication>
 
 #include "todolistdata.h"
 
@@ -11,11 +15,19 @@ class ToDoListAjout : public QWidget
 {
     Q_OBJECT
 protected:
-    QFormLayout *layout;
-    QLineEdit *titre;
-    QLineEdit *objectif;
-    QLineEdit *date;
-    ToDoListData *_data;
+    QFormLayout *_layoutLine;
+    QHBoxLayout *_layoutBtn;
+    QLineEdit *_titre;
+    QLineEdit *_objectif;
+    QDateEdit *_date;
+    QPushButton *_quitBtn;
+    QPushButton *_validerBtn;
+    QVBoxLayout *_layoutPrincipal;
+
+    void initButton();
+    void initLayout();
+    void initLine();
+
 public:
     explicit ToDoListAjout(QWidget *parent = 0);
 
