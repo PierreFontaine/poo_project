3f5b74e4e1dc06e6c028a72dd5c7a19fe0a81b35
Sallio <romane.sallio@gmail.com>
2017-04-16 17:22:41 +0200

    poo miles qui marche

--- projet/miles.cpp
+++ projet/miles.cpp
@@ -1,26 +1,86 @@
 #include "miles.h"
 
+/*
+ author  : Sallio Romane
+ mail    : romane.sallio@gmail.com
+ but     : conv en celsius
+ remarque: instance d'une temperature en miles sans connaitre la valeur
+ precond :
+ postcond:
+ ©2017
+ */
+
 Miles::Miles():Vitesse(){
     setValue(0.0);
 }
 
+/*
+ author  : Sallio Romane
+ mail    : romane.sallio@gmail.com
+ but     : Init de la mesure en connaissant la valeur
+ remarque:
+ precond :
+ postcond:
+ ©2017
+ */
+
 Miles::Miles(double v,subunit t):Vitesse(v,t){
 
 }
+
+/*
+ author  : Sallio Romane
+ mail    : romane.sallio@gmail.com
+ but     : affichage de la mesure avec le bon typage d'unité ici "Mp"
+ remarque:
+ precond :
+ postcond:
+ ©2017
+ */
 
 void Miles::afficher(ostream &flux)const{
     flux << getValue() << "Mp" << endl;
 
 }
 
+/*
+ author  : Sallio Romane
+ mail    : romane.sallio@gmail.com
+ but     : conv en km
+ remarque:
+ precond :
+ postcond:
+ ©2017
+ */
+
 double Miles::getKilometreValue(subunit a)const{
-    return (getMilesValue(a) * 1.609344);
+    return (getValue(a) * 1.609344);
 }
+
+/*
+ author  : Sallio Romane
+ mail    : romane.sallio@gmail.com
+ but     : conv en m
+ remarque:
+ precond :
+ postcond:
+ ©2017
+ */
 
 double Miles::getMetreValue(subunit a)const{
-    return (getMilesValue(a) * 1609.344);
+    return (getValue(a) * 1609.344);
 }
 
+/*
+ author  : Sallio Romane
+ mail    : romane.sallio@gmail.com
+ but     : ici on est deja en mp
+ remarque:
+ precond :
+ postcond:
+ ©2017
+ */
+
 double Miles::getMilesValue(subunit a)const{
-    return (getMilesValue(a));
+    return (getValue(a));
 }
