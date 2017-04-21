062630e23b56d952e3a7df859f91f87cae828478
fontaine pierre <pierre.ftn64@gmail.com>
2017-04-21 10:14:44 +0200

    init layout principal

--- projet/displaynote.cpp
+++ projet/displaynote.cpp
@@ -6,8 +6,8 @@ DisplayNote::DisplayNote(QWidget *parent) : QWidget(parent)
     _heure = new QLabel;
     _note = new QLabel;
     _titre = new QLabel;
-
-    _layoutLine = new QFormLayout;
+    _layoutPrincipal = new QVBoxLayout(this);
+    _layoutLine = new QFormLayout(this);
 
     _layoutLine->addRow("Titre : ",_titre);
     _layoutLine->addRow("Note : ",_note);
@@ -17,14 +17,25 @@ DisplayNote::DisplayNote(QWidget *parent) : QWidget(parent)
     _layoutPrincipal->addLayout(_layoutLine);
 
     setLayout(_layoutPrincipal);
-
+    show();
 }
 
 DisplayNote::DisplayNote(QString d, QString h, QString n, QString t,QWidget *parent) : QWidget(parent){
-   _date = new QLabel(d);
-   _heure = new QLabel(h);
-   _note = new QLabel(n);
-   _titre = new QLabel(t);
+   _date = new QLabel(d,this);
+   _heure = new QLabel(h,this);
+   _note = new QLabel(n,this);
+   _titre = new QLabel(t,this);
+   _layoutPrincipal = new QVBoxLayout(this);
+   _layoutLine = new QFormLayout(this);
 
+
+   _layoutLine->addRow("Titre : ",_titre);
+   _layoutLine->addRow("Note : ",_note);
+   _layoutLine->addRow("Date : ",_date);
+   _layoutLine->addRow("Heure : ",_heure);
+
+   _layoutPrincipal->addLayout(_layoutLine);
+
+   setLayout(_layoutPrincipal);
 }
 
